cc82d26aeea823daa81ae2e248892c1cbd428eb1
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-02-12 15:42:17 +0100

    Add socket.io 2.X client compat flag

--- src/socket.js
+++ src/socket.js
@@ -3,8 +3,10 @@ import { SOCKET_PATH } from './settings.js';
 import io from 'socket.io';
 import { log } from './log.js';
 
+export const socket2Compat = process.env.SOCKET_COMPAT || false;
+
 export const defineSocket = (http) => {
-  const ioServer = io(http, { path: SOCKET_PATH });
+  const ioServer = io(http, { path: SOCKET_PATH, allowEIO3: socket2Compat });
 
   ioServer.on('connection', (socket) => {
     handleC2C(socket, { log: (msg) => log.info(msg) });
